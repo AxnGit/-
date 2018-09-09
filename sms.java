05d018825ad854c7656d80ba384c08d652ef32ea
Axndou <42560597+Axndou@users.noreply.github.com>
2018-09-09 21:27:42 +0800

    第一次使用github
    第一次使用github
    随机生成一个6位的随机数

---
+++ sms.java
@@ -0,0 +1,10 @@
+ public static void main(String[] args) throws ClientException {
+        //随机生成六位随机数
+        StringBuffer stringBuffer=new StringBuffer();
+        for (int x=0;x<=5;x++) {
+            int random = (int) (Math.random() * (10 - 1));
+            stringBuffer.append(random);
+        }
+        String string = stringBuffer.toString();
+        int i = Integer.parseInt(string);
+         System.out.println("OK");
